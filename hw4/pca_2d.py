53db70892f2c29cf3fb3d87ce15b82991943aaba
lambert leong <lamberttleong@gmail.com>
2019-05-08 21:46:28 -1000

    finished and added 3d gaussian vis code

--- hw4/pca_2d.py
+++ hw4/pca_2d.py
@@ -1,35 +1,47 @@
-# import for colormaps
-import numpy as np 
-from matplotlib import pyplot as plt
-import numpy as np
-from matplotlib import cm
-
-x=np.linspace(-10,10, num=100)
-x2=np.linspace(0,20, num=100)
-Y=np.linspace(-10,10, num=100)
-
-x, y = np.meshgrid(x, Y)
-x2, y = np.meshgrid(x2, Y)
-
-z= np.exp(-0.1*x**2-0.1*y**2)
-z2 = np.exp(-0.1*x2**2-0.1*y**2)
-
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#ax.plot_surface(x,y,z, cmap=cm.jet)
-#plt.show()
-
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = Axes3D(fig)
-#ax = Axes3D(fig)
-ax.scatter(x,y,z, c='r', label='pclass 0')
-ax.scatter(x2,y,z2, c='b', label='pclass 0')
-ax.set_xlabel('x', fontsize=30, rotation = 0)
-ax.set_ylabel('y', fontsize=30, rotation = 0)
-ax.set_zlabel('z', fontsize=30, rotation = 0)
-#ax.scatter(x,y,z c='b', label = 'pclass 1')
-plt.legend()
-ax.view_init(90, 180+90)
-plt.show()
 
+import numpy as np
+from scipy.stats import multivariate_normal
+
+x, y = np.mgrid[-1.0:1.0:30j, -1.0:1.0:30j]
+x2, y2 = np.mgrid[-1.0:1.0:30j, -1.0:1.0:30j]
+
+# Need an (N, 2) array of (x, y) pairs.
+xy = np.column_stack([x.flat, y.flat])
+xy2 = np.column_stack([x2.flat, y2.flat])
+
+mu = np.array([0.0, 0.0])
+mu2 = np.array([0.0, 1.0])
+
+sigma = np.array([.5, .5])
+covariance = np.diag(sigma**2)
+
+sigma2 = np.array([.5, .5])
+covariance2 = np.diag(sigma**2)
+
+z = multivariate_normal.pdf(xy, mean=mu, cov=covariance)
+z2 = multivariate_normal.pdf(xy2, mean=mu2, cov=covariance)
+
+# Reshape back to a (30, 30) grid.
+z = z.reshape(x.shape)
+z2 = z2.reshape(x2.shape)
+
+
+
+
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+
+
+ax.plot_surface(x,y,z)
+ax.plot_surface(x2,y2,z2)
+ax.set_xlabel('PC 1')
+ax.set_ylabel('PC 2')
+ax.set_zlabel('PC 3')
+#ax.plot_wireframe(x,y,z)
+
+plt.show()
